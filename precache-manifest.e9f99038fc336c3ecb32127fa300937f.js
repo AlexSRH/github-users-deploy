52dd2fab81cded09b3f5436754c14e656a3c2f60
Alexsandro G Bezerra <aalexo1313@gmail.com>
2020-05-21 21:14:47 -0300

    Updates

---
+++ precache-manifest.e9f99038fc336c3ecb32127fa300937f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b175ac901631cedeb4fd94dddd4ffa",
+    "url": "/github-users-deploy/index.html"
+  },
+  {
+    "revision": "4c333d3cb11d9814a87a",
+    "url": "/github-users-deploy/static/js/2.0c9864f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-users-deploy/static/js/2.0c9864f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506f79f1267d377747b0",
+    "url": "/github-users-deploy/static/js/main.2e72b365.chunk.js"
+  },
+  {
+    "revision": "4e0cd094446329b5bb0c",
+    "url": "/github-users-deploy/static/js/runtime-main.5600d34b.js"
+  }
+]);
